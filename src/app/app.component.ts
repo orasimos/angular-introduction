864c2a22368c3dc026471480432fae2cb842e6e6
O. Asimopoulos <orasimos@gmail.com>
2023-04-03 19:32:03 +0300

    laptop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SideBar } from './app.interfaces';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-introduction';
+  title = 'Angular Introduction';
+
+  sideBarMenu: SideBar[] = [
+    { text: 'Greeting', path: 'greeting' },
+    { text: 'One way bind', path: 'one-way-bind' },
+    { text: 'Event bind', path: 'event-bind' },
+    { text: 'Two way bind', path: 'two-way-bind' },
+    { text: 'Structural directives', path: 'structural-directives' },
+    { text: 'Template variables', path: 'template-variables' },
+    { text: 'Component input', path: 'input' },
+    { text: 'Component input-output', path: 'input-output' }
+  ]
 }
